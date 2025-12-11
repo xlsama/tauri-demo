57e6a7d4700e6181375e8728642b8706be0bb0c4
xlsama <im.xlsama@gmail.com>
2025-12-11 17:05:11 +0800

    chore: add animate

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import Sidebar from '@/components/Sidebar.vue'
+import { AnimatePresence, Motion } from 'motion-v'
 import { invoke } from '@tauri-apps/api/core'
 
 const greetMsg = ref('')
@@ -17,7 +18,19 @@ async function greet() {
     <div class="h-dvh overflow-hidden flex">
       <Sidebar />
       <main class="flex-1 overflow-y-auto p-4">
-        <RouterView />
+        <RouterView v-slot="{ Component, route }">
+          <AnimatePresence mode="wait">
+            <Motion
+              v-if="Component"
+              :key="route.path"
+              :initial="{ opacity: 0 }"
+              :animate="{ opacity: 1 }"
+              :transition="{ duration: 0.25 }"
+            >
+              <component :is="Component" />
+            </Motion>
+          </AnimatePresence>
+        </RouterView>
       </main>
     </div>
   </UApp>
