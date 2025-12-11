b5e84d44b88ba10603e20edef4c522e2d6078ff5
xlsama <im.xlsama@gmail.com>
2025-12-11 17:07:46 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import Sidebar from '@/components/Sidebar.vue'
 import { AnimatePresence, Motion } from 'motion-v'
-import { invoke } from '@tauri-apps/api/core'
-
-const greetMsg = ref('')
-const name = ref('')
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke('greet', { name: name.value })
-}
 </script>
 
 <template>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { invoke } from '@tauri-apps/api/core'
+import { ref } from 'vue'
+
+const greetMsg = ref('')
+const name = ref('')
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsg.value = await invoke('greet', { name: name.value })
+}
+</script>
 
 <template>
-  <h1 class="text-2xl font-bold">Home page</h1>
+  <div class="space-y-4">
+    <h1 class="text-2xl font-bold">Home page</h1>
+    <div class="space-x-2">
+      <UInput v-model="name" placeholder="Enter your name" />
+      <UButton @click="greet">Greet</UButton>
+    </div>
+    <p>{{ greetMsg }}</p>
+  </div>
 </template>
